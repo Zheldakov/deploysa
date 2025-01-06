9e7c67b4e3f0ef55868dd3c1491468abdeadce78
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-06 23:41:46 +0300

    Добавлено отображение пользователей в админ-панели django

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from users.models import User
+# Модель User в административной панели Django
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Отображение в админ панели пользователей"""
+    list_display = ('id','role','email','first_name','last_name','phone','telegram', 'is_active') # Показываем поля в списке модели
+    list_filter = ('id','email','role',) # Фильтрация
+    ordering = ('id',)
