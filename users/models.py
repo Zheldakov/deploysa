34a74259db0e3cf3e4cbd8f5543625a5630687ba
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-06 23:44:55 +0300

    Создание моделей ролей пользователей и пользователей

--- users/models.py
+++ users/models.py
@@ -1,3 +1,36 @@
+# Описание моделей
 from django.db import models
+from django.contrib.auth.models import  AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+# настройка полей, чтобы возможно было заполнить поля пустыми
+NULLABLE ={'blank': True, 'null': True}
+
+class UserRoles(models.TextChoices):
+    """ Роли пользователь """
+    ADMIN = 'admin', _('admin')
+    MODERATOR = 'moderator', _('moderator')
+    USER = 'user', _('user')
+
+class User(AbstractUser):
+    """Модель пользователя с полями email, телефон и Telegram username."""
+    username = None
+    email = models.EmailField(unique=True,verbose_name='email')
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.USER)
+    first_name = models.CharField(max_length=150, verbose_name='First Name',default="Anonymous")
+    last_name = models.CharField(max_length=150, verbose_name='Last Name', default="Anonymous")
+    phone = models.CharField(max_length=35, verbose_name='Phone number', **NULLABLE)
+    telegram = models.CharField(max_length=150, verbose_name='Telegram Username', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Avatar', **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name='active', help_text='Активный пользователь')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return  f'{self.email}'
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural ='User'
+        ordering = ['id']
