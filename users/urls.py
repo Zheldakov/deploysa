6c86187c92bddac0a2ca2e8bf4be2c7b7990fb54
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-07 23:01:12 +0300

    Добавлены url лдя выхода

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import user_login_view
+from users.views import user_login_view,user_logout_view
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', user_login_view, name="login_user"), #  url стартовой страницы для входа
+    path('logout/', user_logout_view, name="logout_user"),
 ]
