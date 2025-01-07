ebc8cd8badc64a2ae3789712a94acf81c4d69de8
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-07 19:22:51 +0300

    Добавлено приложение technic
    Добавлено user.views для выхода пользователя

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # добавленные приложения
     'users',
+    'technic',
 ]
 
 # плагины и библиотеки
--- users/views.py
+++ users/views.py
@@ -10,23 +10,30 @@ from users.forms import UserLoginForm
 
 def user_login_view(request):
     # Выводим форму логина
+    confirmation=True
     if request.method == 'POST':
         form = UserLoginForm(request.POST)  # Создаем экземпляр формы
         if form.is_valid(): # Если форма валидна
             cd = form.cleaned_data  #Получаем данные email и password и очищаем форму
             user = authenticate(email=cd['email'], password=cd['password'])  # Аутентифицируем пользователя
-            print("IDI NA HUI2", user)
             if user is not None:  # Если пользователь существует
                 if user.is_active:  # Если пользователь активен
                     login(request, user)  # Авторизуем пользователя
-                    return HttpResponseRedirect(reverse('main:index'))  # Переход на главную страницу с техникой
+                    return HttpResponseRedirect(reverse('technic:technic_list'))  # Переход на главную страницу с техникой
 
                 else:
                     return HttpResponse('Аккаунт не активен')
             else:
-                return HttpResponse('Не верно введены данные')
+                confirmation = False
 
     form = UserLoginForm()
     context = {
-        'form': form}
+        'form': form,
+        'user': confirmation}
     return render(request, 'users/login_user.html', context)
+
+def user_logout_view(request):
+    # выход из системы
+    print("выход из системы")
+    logout(request)
+    return redirect('users:login_user')
