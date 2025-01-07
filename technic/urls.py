78f3ee384edbf94a2af81c4abef8c63707269459
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-08 00:38:23 +0300

    Исправление app_name

--- technic/urls.py
+++ technic/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from users.apps import UsersConfig
+from technic.apps import TechnicConfig
 from technic.views import technic_list_view
 
-app_name = UsersConfig.name
+app_name = TechnicConfig.name
 
 urlpatterns = [
     path('list/', technic_list_view, name="technic_list"), #  url список всей техники
