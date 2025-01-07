a39eb949cd52452dc05d12f8746365e22a04131f
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-07 19:19:11 +0300

    Созданы urls авторизации пользователя (стартовая страница)

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from users.apps import UsersConfig
+from users.views import user_login_view
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('', user_login_view, name="login_user"), #  url стартовой страницы для входа
+]
