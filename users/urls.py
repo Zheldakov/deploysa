7b93a86e155746b954464fa772f9653bc22b2e4a
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-10 03:56:55 +0300

    Добавлены urls:  - для отображения профиля;  - для редактирования профиля и пароля.

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import user_login_view,user_logout_view
+from users.views import user_login_view,user_logout_view,UserUpdateView,UserPasswordChangeView,UserProfileView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', user_login_view, name="login_user"), #  url стартовой страницы для входа
     path('logout/', user_logout_view, name="logout_user"),
+    path('profile/', UserProfileView.as_view(), name='profile_user'),
+    path('update/', UserUpdateView.as_view(), name="update_user"),
+    path('change_password/', UserPasswordChangeView.as_view(), name='change_password_user'),
 ]
