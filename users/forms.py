031f7d30ee35ee4c757df32bf466136e2cce196a
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-10 03:51:06 +0300

    Добавлены/отредактированы формы:  - для отображения профиля;  - обновление профиля и пароля.

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,20 @@
 from django import forms
 from .models import User
-from django.contrib.auth.forms import AuthenticationForm
+
+from django.contrib.auth import password_validation
+from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.forms import ModelForm
 
+from users.validators import validate_password
 
 
-# class StyleFromMixin:
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         for field_name, field in self.fields.items():
-#             field.widget.attrs['class'] = 'form-control'
+
+class StyleFromMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
 class UserLoginForm(forms.Form):
     """ Форма для авторизации пользователя."""
@@ -21,6 +26,14 @@ class UserLoginForm(forms.Form):
         'class': 'form-control',
        'placeholder': 'Пароль'
     }))
+
+class UserForm(StyleFromMixin, forms.ModelForm):
+    """ Форма пользователя."""
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email',
+                  'phone', 'telegram', 'avatar',)
 #
 
 # class UserLoginForm(ModelForm):
@@ -58,3 +71,18 @@ class UserLoginForm(forms.Form):
 #         model = User
 #         fields = ['email', 'password']
 
+class UserPasswordChangeForm(StyleFromMixin, PasswordChangeForm):
+    """ Форма для смены пароля."""
+
+    def clean_new_password2(self):
+        # Проверка соответствия паролей
+        password1 = self.cleaned_data.get("new_password1")
+        password2 = self.cleaned_data.get("new_password2")
+        validate_password(password1)
+        if password1 and password2 and password1 != password2:
+            raise ValidationError(
+                self.error_messages["password_mismatch"],
+                code="password_mismatch",
+                )
+        password_validation.validate_password(password2, self.user)
+        return password2
