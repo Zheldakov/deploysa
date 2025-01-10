881ad041c4b293f7a9f5bd6d65c38f3754e3bfcc
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-10 03:57:20 +0300

    Добавлены views:  - для отображения профиля;  - для редактирования профиля и пароля.

--- users/views.py
+++ users/views.py
@@ -1,11 +1,19 @@
+from gc import get_objects
+
+from django.contrib.auth.views import PasswordChangeView
 from django.shortcuts import reverse, render, redirect
 
 
 from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, user_login_failed
 from django.contrib.auth.decorators import login_required
+from django.template.context_processors import request
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView, DetailView
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserForm, UserPasswordChangeForm
+from users.models import User
+
 
 
 def user_login_view(request):
@@ -39,3 +47,62 @@ def user_logout_view(request):
     print("выход из системы")
     logout(request)
     return redirect('users:login_user')
+
+class UserProfileView(DetailView):
+    """ Просмотр профиля пользователя."""
+    model = User
+    form_class = UserForm
+    template_name = 'users/profile_user.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def get_context_data(self, **kwargs):
+        # Добавляем данные в extra_context
+        context_data = super().get_context_data(**kwargs)
+        object = self.get_object()
+        context_data['title'] = f'Профиль пользователя'
+        context_data['user_name'] = f'{object.first_name}'
+        context_data['user_email'] = f'{object.email}'
+        return context_data
+
+
+
+class UserUpdateView(UpdateView):
+    """ Изменение профиля пользователя."""
+    model = User
+
+    form_class = UserForm
+    template_name = 'users/update_user.html'
+    success_url = reverse_lazy('users:profile_user')
+
+    def get_object(self, queryset=None):
+        # Получаем пользователя
+        return self.request.user
+
+    def get_context_data(self, **kwargs):
+        # Добавляем данные в extra_context
+        context_data = super().get_context_data(**kwargs)
+        object = self.get_object()
+        context_data['title'] = f'Редактирование профиля'
+        context_data['user_name'] = f'{object.first_name}'
+        context_data['user_email'] = f'{object.email}'
+        return context_data
+
+class UserPasswordChangeView(PasswordChangeView):
+    """ Изменение пароля пользователя."""
+    form_class = UserPasswordChangeForm
+    template_name = 'users/update_user.html'
+    success_url = reverse_lazy('technic:technic_list')
+    def get_object(self, queryset=None):
+        # Получаем пользователя
+        return self.request.user
+
+    def get_context_data(self, **kwargs):
+        # Добавляем данные в extra_context
+        context_data = super().get_context_data(**kwargs)
+        object = self.get_object()
+        context_data['title'] = f'Редактирование пароля'
+        context_data['user_name'] = f'{object.first_name}'
+        context_data['user_email'] = f'{object.email}'
+        return context_data
