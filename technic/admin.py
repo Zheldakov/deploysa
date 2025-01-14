bd02f8bd4189e3ff0c959b8371135d7b7fdafd5e
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-14 20:53:28 +0300

    Корректировка.

--- technic/admin.py
+++ technic/admin.py
@@ -1,21 +1,24 @@
 from django.contrib import admin
-from technic.models import Technic,Department,TypeTechnic
+from technic.models import Technic, Department, TypeTechnic
 
 
 @admin.register(TypeTechnic)
 class TypeTechnicAdmin(admin.ModelAdmin):
     """Отображение в админ панели типа техники"""
-    list_display = ('pk', 'name') # показываем поля в списке модели
-    ordering = ('pk', 'name') # сортировка по первому полю id и имени
+    list_display = ('pk', 'name')  # показываем поля в списке модели
+    ordering = ('pk', 'name')  # сортировка по первому полю id и имени
+
+
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
     """Отображение в админ панели подразделения"""
-    list_display = ('pk', 'name') # показываем поля в списке модели
-    ordering = ('pk', 'name') # сортировка по первому полю id и имени
+    list_display = ('pk', 'name')  # показываем поля в списке модели
+    ordering = ('pk', 'name')  # сортировка по первому полю id и имени
+
 
 @admin.register(Technic)
 class TechnicAdmin(admin.ModelAdmin):
     """Отображение в админ панели техники"""
-    list_display = ('name', 'number','type', 'imei','invnumber') # показываем поля в списке модели
-    list_filter = ('type',) # фильтрация по полю type
-    ordering = ('name',) # сортировка по имени
+    list_display = ('name', 'number', 'type', 'imei', 'invnumber')  # показываем поля в списке модели
+    list_filter = ('type',)  # фильтрация по полю type
+    ordering = ('name',)  # сортировка по имени
