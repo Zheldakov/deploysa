45e3d300f75369e9fe32280acb4c62f3c9631124
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-14 14:16:36 +0300

    Добавлены модели техники, типа техники, подразделение техники.

--- technic/admin.py
+++ technic/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from technic.models import Technic,Department,TypeTechnic
 
-# Register your models here.
+
+@admin.register(TypeTechnic)
+class TypeTechnicAdmin(admin.ModelAdmin):
+    """Отображение в админ панели типа техники"""
+    list_display = ('pk', 'name') # показываем поля в списке модели
+    ordering = ('pk', 'name') # сортировка по первому полю id и имени
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    """Отображение в админ панели подразделения"""
+    list_display = ('pk', 'name') # показываем поля в списке модели
+    ordering = ('pk', 'name') # сортировка по первому полю id и имени
+
+@admin.register(Technic)
+class TechnicAdmin(admin.ModelAdmin):
+    """Отображение в админ панели техники"""
+    list_display = ('name', 'number','type', 'imei','invnumber') # показываем поля в списке модели
+    list_filter = ('type',) # фильтрация по полю type
+    ordering = ('name',) # сортировка по имени
--- technic/models.py
+++ technic/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+# настройка полей, чтобы возможно было заполнить поля пустыми
+NULLABLE = {'blank': True, 'null': True}
+
+
+class TypeTechnic(models.Model):
+    """Тип техники"""
+    name = models.CharField(max_length=100, verbose_name='type', **NULLABLE)  # `Имя типа техники
+    description = models.CharField(max_length=150, verbose_name='description', **NULLABLE)  # Описание
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тип техники'
+        verbose_name_plural = 'Типы техники'
+
+class Department(models.Model):
+    """Подразделение техники"""
+    name = models.CharField(max_length=100, verbose_name='type', **NULLABLE)  # Имя подразделения
+    description = models.CharField(max_length=150, verbose_name='description', **NULLABLE)  # Описание
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Подразделение'
+        verbose_name_plural = 'Подразделения'
+
+
+class Technic(models.Model):
+    """Техника"""
+    name = models.CharField(max_length=250, verbose_name='Наименование', **NULLABLE)  # Наименование техники
+    type = models.ForeignKey(TypeTechnic, on_delete=models.CASCADE, verbose_name='Тип')  # Тип техники
+    photo = models.ImageField(upload_to='technic/', verbose_name='Изображение', **NULLABLE)  # Фотография техники
+    number = models.CharField(max_length=8, verbose_name='Гос. номер', **NULLABLE) # Государственный номер
+    invnumber = models.IntegerField(verbose_name="Инвентарный номер", **NULLABLE) #Инвентарный номер
+    imei = models.IntegerField(verbose_name='IMEI', **NULLABLE) # номер IMEI
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, verbose_name='Подразделение')  # Подразделение техники
+
+    is_active = models.BooleanField(default=True, verbose_name="Активность")
+
+
+    def __str__(self):
+        return f'{self.name} ({self.number})'  #техника в формате "Наименование техники (гос.Номер)"
+
+    class Meta:
+        verbose_name = 'Техника'
+        verbose_name_plural = 'Техника'
+
+
+class Service(models.Model):
+    """Техническое обслуживание"""
+    technic = models.ForeignKey(Technic, on_delete=models.CASCADE) # Техника на которой проведен сервис
+    work = models.CharField(max_length=100, verbose_name='Работа') # Проведенная работа
+    date = models.DateField(**NULLABLE, verbose_name='Дата выполнения') # Дата проведения работы
+    description = models.CharField(max_length=250, verbose_name='Описание работ', **NULLABLE)  # Описание работы
+
+
+    def __str__(self) -> str:
+        return f'{self.technic} - {self.work}'
+
+    class Meta:
+        verbose_name = 'Тех. обслуживание'
+        verbose_name_plural = 'Тех. обслуживания'
