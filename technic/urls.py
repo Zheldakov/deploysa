665fc0adb423451badde266104a2b90732259609
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-14 14:16:54 +0300

    Добавлены urls для техники, типа техники, подразделение техники.

--- technic/urls.py
+++ technic/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from technic.apps import TechnicConfig
-from technic.views import technic_list_view
+from technic.views import TechnicListView, DepartmentListView, TypeListView
 
 app_name = TechnicConfig.name
 
 urlpatterns = [
-    path('list/', technic_list_view, name="technic_list"), #  url список всей техники
+    path('list/', TechnicListView.as_view(), name="technic_list"), #  url список всей техники
+    path('type/', TypeListView.as_view(), name="type_list"), #  url список всей тех
+    path('departament/', DepartmentListView.as_view(), name="department_list"), #  url список всей тех
    ]
