7d94fb5aa5aa0130c8fe3e76c5f0f8930b242568
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-14 14:17:05 +0300

    Добавлены views для техники, типа техники, подразделение техники.

--- technic/views.py
+++ technic/views.py
@@ -1,13 +1,50 @@
+
+
 from django.shortcuts import reverse, render, redirect
+from django.views.generic import ListView
 
-def technic_list_view(request):
-    """ Показывает страницу со списком всей техники."""
-    user = request.user
+from technic.models import Technic, TypeTechnic, Department
 
-    context = {
-        'title': "Вся техника",
-        'user_name': user.first_name,
-        'user_email': user.email,
+
+# def technic_list_view(request):
+#     """ Показывает страницу со списком всей техники."""
+#     user = request.user
+#     context = {
+#         'title': "Вся техника",
+#         'user_name': user.first_name,
+#         'user_email': user.email,
+#     }
+#     return render(request, 'technic/technic_list.html', context)  # тестовый вариант
+
+class TechnicListView(ListView):
+    """ Список всей техники."""
+    model = Technic
+    paginate_by = 6
+    template_name = 'technic/technic_list.html'
+    extra_context = {
+        'title': f'Техника'
     }
 
-    return render(request, 'technic/technic_list.html', context)  # тестовый вариант
+
+# class TypeListView(ListView):
+#     """ Показывает страницу с информацией о техники определенного типа."""
+#     model = TypeTechnic
+#     template_name = 'technic/type_list.html'
+#
+#     def get_queryset(self):
+#         # Фильтр показывает только активную определенного типа технику
+#         queryset = super().get_queryset().filter(
+#             typetechnic_id=self.kwargs.get('pk'), is_active=True
+#         )
+#         return queryset
+#
+class DepartmentListView(ListView):
+    """ Показывает страницу с информацией о техники определенного подразделения"""
+    model = Department
+    template_name = 'technic/dep_type_list.html'
+
+
+class TypeListView(ListView):
+    """ Показывает страницу с информацией о техники определенного типа"""
+    model = TypeTechnic
+    template_name = 'technic/dep_type_list.html'
