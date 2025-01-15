ddb81d1224bd3cc5209d5a94df8d9e1f4d2e8b35
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-15 15:55:31 +0300

    Добавлены urls для CRUD техники.

--- technic/urls.py
+++ technic/urls.py
@@ -3,15 +3,17 @@ from django.urls import path
 from technic.apps import TechnicConfig
 from technic.views import TechnicListView, DepartmentListView, TypeListView, TypeTechnicUpdateView, \
     DepartmentUpdateView, TypeTechnicDeleteView, DepartmentDeleteView, DepartmentCreateView, TypeTechnicCreateView, \
-    TechnicDetailView
+    TechnicDetailView, TechnicUpdateView, TechnicDeleteView
 
 app_name = TechnicConfig.name
 
 urlpatterns = [
-    path('list/', TechnicListView.as_view(), name="technic_list"),  # url список всей техники
-    path('detail/<int:pk>', TechnicDetailView.as_view(), name="technic_detail"),  # url список всей техники
-    path('type/', TypeListView.as_view(), name="type_list"),  # url список всех типов техники
-    path('department/', DepartmentListView.as_view(), name="department_list"),  # url список всех подразделений
+    path('list/', TechnicListView.as_view(), name="technic_list"),  # список всей техники
+    path('detail/<int:pk>', TechnicDetailView.as_view(), name="technic_detail"),  # детальная информация по техники
+    path('update/<int:pk>', TechnicUpdateView.as_view(), name="technic_update"),  # редактирование техники
+    path('delete/<int:pk>', TechnicDeleteView.as_view(), name="technic_delete"),  # удаление техники
+    path('type/', TypeListView.as_view(), name="type_list"),  # список всех типов техники
+    path('department/', DepartmentListView.as_view(), name="department_list"),  # список всех подразделений
     path('type/create/', TypeTechnicCreateView.as_view(), name='type_create'), # создание типов техники
     path('department/create/', DepartmentCreateView.as_view(), name='department_create'), # создание подразделений
     path('type/update/<int:pk>/', TypeTechnicUpdateView.as_view(), name='type_update'), # редактирование типа техники
