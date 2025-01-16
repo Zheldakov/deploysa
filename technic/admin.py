85e1aa3f0dda17fcc18a2e423823bf6e5a9d4d2f
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-17 01:23:34 +0300

    Регистрация в панели администратора сервисных работ.

--- technic/admin.py
+++ technic/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from technic.models import Technic, Department, TypeTechnic
+from technic.models import Technic, Department, TypeTechnic, Service
 
 
 @admin.register(TypeTechnic)
@@ -22,3 +22,8 @@ class TechnicAdmin(admin.ModelAdmin):
     list_display = ('name', 'number', 'type', 'imei', 'invnumber')  # показываем поля в списке модели
     list_filter = ('type',)  # фильтрация по полю type
     ordering = ('name',)  # сортировка по имени
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    """Отображение в админ панели сервисных работ на технике"""
+    list_display = ('technic','work','date','description')  # показываем поля в списке модели
