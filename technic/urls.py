013e598c70f197f70554f293901cb5d481702753
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-17 01:24:31 +0300

    Добавление urls сервисных работ.

--- technic/urls.py
+++ technic/urls.py
@@ -4,7 +4,7 @@ from technic.apps import TechnicConfig
 from technic.views import TechnicListView, DepartmentListView, TypeListView, TypeTechnicUpdateView, \
     DepartmentUpdateView, TypeTechnicDeleteView, DepartmentDeleteView, DepartmentCreateView, TypeTechnicCreateView, \
     TechnicDetailView, TechnicUpdateView, TechnicDeleteView, TechnicCreateView, TechnicListFilterDepartmentView, \
-    TechnicListFilterTypeTechnicView, TechnicSearchListView
+    TechnicListFilterTypeTechnicView, TechnicSearchListView, TechnicServiceUpdateView, ServiceFilterTechnicView
 
 app_name = TechnicConfig.name
 
@@ -13,9 +13,9 @@ urlpatterns = [
     path('department/<int:pk>/technic/', TechnicListFilterDepartmentView.as_view(), name='department_technic'), # техника определенного подразделения
     path('type/<int:pk>/technic/', TechnicListFilterTypeTechnicView.as_view(), name='type_technic'), # техника определенного типа
     path('create/', TechnicCreateView.as_view(), name="technic_create"),  # добавление техники
-    path('detail/<int:pk>', TechnicDetailView.as_view(), name="technic_detail"),  # детальная информация по техники
-    path('update/<int:pk>', TechnicUpdateView.as_view(), name="technic_update"),  # редактирование техники
-    path('delete/<int:pk>', TechnicDeleteView.as_view(), name="technic_delete"),  # удаление техники
+    path('detail/<int:pk>/', TechnicDetailView.as_view(), name="technic_detail"),  # детальная информация по техники
+    path('update/<int:pk>/', TechnicUpdateView.as_view(), name="technic_update"),  # редактирование техники
+    path('delete/<int:pk>/', TechnicDeleteView.as_view(), name="technic_delete"),  # удаление техники
     path('type/', TypeListView.as_view(), name="type_list"),  # список всех типов техники
     path('department/', DepartmentListView.as_view(), name="department_list"),  # список всех подразделений
     path('type/create/', TypeTechnicCreateView.as_view(), name='type_create'), # создание типов техники
@@ -25,4 +25,6 @@ urlpatterns = [
     path('type/delete/<int:pk>/', TypeTechnicDeleteView.as_view(), name='type_delete'), # удаление типа техники
     path('department/delete/<int:pk>/', DepartmentDeleteView.as_view(), name='department_delete'), # удаление подразделений
     path('number/search/', TechnicSearchListView.as_view(), name='technic_search'), # поиск техники по гос номеру
+    path('service/<int:pk>/', TechnicServiceUpdateView.as_view(), name='technic_service_update'), # сервис техники
+    path('/<int:pk>/service', ServiceFilterTechnicView.as_view(), name='technic_service_list'), # список сервисов по техники
 ]
