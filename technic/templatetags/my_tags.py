c7508b9347eab458ab124f037966e9186d9ecb64
Zheldakov Andrey <zheldakov.aa@gmail.com>
2025-01-26 20:19:35 +0300

    Редактирование комментариев.

--- technic/templatetags/my_tags.py
+++ technic/templatetags/my_tags.py
@@ -13,18 +13,18 @@ def user_media(val):
     """Фильтр для замены пути к медиафайлам пользователей на URL адрес из настроек MEDIA_URL."""
     if val:
         return fr"/media/{val}" # Если путь к медиафайлу пользователя не пустой, возвращаем его URL адрес
-    return '/static/noavatar.png' # Если путь к медиафайлу пользователя пустой, возвращаем статический дефолтный изображение
+    return '/static/noavatar.png' # Если путь к медиафайлу пользователя пустой, возвращаем дефолтное изображение
 
 @register.filter()
 def technic_media(val):
-    """Фильтр для замены пути к медиафайлам пользователей на URL адрес из настроек MEDIA_URL."""
+    """Фильтр для замены пути к медиафайлам техники на URL адрес из настроек MEDIA_URL."""
     if val:
-        return fr"/media/{val}" # Если путь к медиафайлу техники не пустой, возвращаем его URL адрес
-    return '/static/notechnic.svg' # Если путь к медиафайлу техники пустой, возвращаем статический дефолтный изображение
+        return fr"/media/{val}"
+    return '/static/notechnic.svg'
 
 @register.filter()
 def service_media(val):
     """Фильтр для замены пути к медиафайлам сервисных работ на URL адрес из настроек MEDIA_URL."""
     if val:
-        return fr"/media/{val}" # Если путь к медиафайлу техники не пустой, возвращаем его URL адрес
-    return '/static/service.jpg' # Если путь к медиафайлу техники пустой, возвращаем статический дефолтный изображение
+        return fr"/media/{val}"
+    return '/static/service.jpg'
